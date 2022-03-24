19d74576387642797a4926a37f57e90972e011e6
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-03-24 05:51:21 -1000

    Initial commit, Fizzbuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+// Write your solution below this line:
+
+for(i = 1; i <= 50; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("fizzbuzz");
+    }else if (i % 3 === 0){
+        console.log("buzz");
+    }else if (i % 5 === 0){
+        console.log("fizz");
+    }else{
+        console.log(i);
+    }
+};
